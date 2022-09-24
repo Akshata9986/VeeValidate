b48f31760e82f4ae9883c5c0c7e158113e00eef5
Akshata9986 <akshatanaik9164@gmail.com>
2022-09-24 12:31:44 +0530

    vee_validate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <vee-validate />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VeeValidate from "./components/VeeValidate.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    VeeValidate,
+  },
+};
 </script>
 
 <style>
